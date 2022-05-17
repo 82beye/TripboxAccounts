d2a71101e98cfcb3f2156ee96fe4d0cb23957e8a
82beye <82beye@gmail.com>
2022-05-17 11:57:55 +0900

    no message

---
+++ Tripbox.Accounts.API/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tripbox.Accounts.API.Controllers
+{
+
+    public static class HttpContextExtensions
+    {
+        public static async Task<AuthenticationScheme[]> GetExternalProvidersAsync(this HttpContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var schemes = context.RequestServices.GetRequiredService<IAuthenticationSchemeProvider>();
+
+            return (from scheme in await schemes.GetAllSchemesAsync()
+                    where !string.IsNullOrEmpty(scheme.DisplayName)
+                    select scheme).ToArray();
+        }
+
+        public static async Task<bool> IsProviderSupportedAsync(this HttpContext context, string provider)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var obj = (from scheme in await context.GetExternalProvidersAsync()
+                       where string.Equals(scheme.Name, provider, StringComparison.OrdinalIgnoreCase)
+                       select scheme);
+
+            return (from scheme in await context.GetExternalProvidersAsync()
+                    where string.Equals(scheme.Name, provider, StringComparison.OrdinalIgnoreCase)
+                    select scheme).Any();
+        }
+    }
+}
